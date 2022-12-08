eb7b9b4898f6b564a872854afcde3459f6ad7461
Enric Bisbe Gil <enricu@gmail.com>
2022-12-08 11:33:01 +0100

    Add CounterButtons test

--- src/countStore.js
+++ src/countStore.js
@@ -1,15 +1,24 @@
 import { defineStore } from 'pinia'
+import Vue from 'vue'
 
 export const useCountStore = defineStore('count', {
   state: () => ({
-    total: 0
+    total: 0,
+    syncStatus: {}
   }),
+  getters: {
+    totalValue: (state) => state.syncStatus.total
+  },
   actions: {
     increment() {
-      this.total++
+      this.total ++ 
+      if(!this.syncStatus.total) Vue.set(this.syncStatus, 'total', {value: 0})
+      this.syncStatus.total.value++
     },
     decrement() {
-      this.total--
+      this.total --
+      if(!this.syncStatus.total) Vue.set(this.syncStatus, 'total', {value: 0})
+      this.syncStatus.total.value--
     }
   },
   persist: { enabled: true }
--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,20 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'prompt',
-      devOptions: {
-        enabled: true
-      }
-    })
+    vue()
+    // VitePWA({
+    //   registerType: 'prompt',
+    //   devOptions: {
+    //     enabled: true
+    //   }
+    // })
   ],
   resolve: {
     alias: {
       '@': resolve('src')
     }
   },
+  server: { port: 1234 },
   preview: { open: true },
   build: { target: 'esnext' }
 })
